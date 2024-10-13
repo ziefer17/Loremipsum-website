94eb9a5be0bce2cffb303e613e0d252de43bc102
ziefer17 <duothehiep@gmail.com>
2024-10-14 05:02:51 +0700

    Homepage
    cần chỉnh sửa lại footer và thanh tìm kiếm

--- ProjectLoremipsu/Controllers/HomeController.cs
+++ ProjectLoremipsu/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace ProjectLoremipsu.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult TestHomepage()
+        {
+            return View();
+        }
         public ActionResult Index()
         {
             return View();
