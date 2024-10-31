66d564da8c171cb8d2ff0f617e28d0248805daae
ziefer17 <duothehiep@gmail.com>
2024-10-31 08:43:48 +0700

    Add Authentication
    chưa hoàn thiện authentication

--- ProjectLoremipsu/Properties/AssemblyInfo.cs
+++ ProjectLoremipsu/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("99e04a47-9ce3-40d2-b208-660b9f5905ae")]
+[assembly: Guid("3d0dd1b3-db0c-4387-bbed-2828e5dbca1e")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ ProjectLoremipsu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectLoremipsu.Startup))]
+namespace ProjectLoremipsu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
