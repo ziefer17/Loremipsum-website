542dfc021b15eef2cb1315be9194a00a7584017c
Fivena <vinhvinhquoctrieu231@gmail.com>
2024-11-07 11:30:04 +0700

    Update user.cs

--- ProjectLoremipsu/Models/user.cs
+++ ProjectLoremipsu/Models/user.cs
@@ -11,7 +11,8 @@ namespace ProjectLoremipsu.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class user
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,13 +23,28 @@ namespace ProjectLoremipsu.Models
         }
     
         public int user_id { get; set; }
+        [Required]
+        [Display(Name = "User Name")]
         public string name { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string email { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
         public string phone { get; set; }
         public Nullable<System.DateTime> created_date { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string password { get; set; }
         public string role { get; set; }
-    
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("password", ErrorMessage = "Password does not match")]
+        [Display(Name = "Confirm Password")]
+        public string confirmPassword { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<property> properties { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
